6a9177951b85d04eb6e8e7ee954abfa52e25dc1e
icmtsan <penelopa.armstrong@gmail.com>
2025-06-26 00:14:10 +0300

    New task

---
+++ Variables-types/task.js
@@ -0,0 +1,14 @@
+let someValue;
+console.log(typeof someValue);
+
+someValue = 11;
+console.log(typeof someValue);
+
+someValue = 'cool value';
+console.log(typeof someValue);
+
+someValue = null;
+console.log(typeof someValue);
+
+someValue = true;
+console.log(typeof someValue);
