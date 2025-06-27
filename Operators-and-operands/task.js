74c72c7157e8a12f459f0ca562c2b50287616224
icmtsan <penelopa.armstrong@gmail.com>
2025-06-27 16:35:52 +0300

    New task

---
+++ Operators-and-operands/task.js
@@ -0,0 +1,19 @@
+let     x = 7;
+
+if (x%2 != 0){
+    console.log("число нечетное")
+} else {
+    console.log("число четное")
+}
+
+
+let name;
+name = "Tasha";
+//name = "";
+//name = null;
+
+if(name === "" || name === undefined || name === null){
+    console.log("Hello, Guest!")
+} else {
+    console.log(`Hello, ${name}`)
+}
