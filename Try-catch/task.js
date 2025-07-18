ed3a409f88f34741f7b9ce5d1ae4c84dddff7fd4
icmtsan <penelopa.armstrong@gmail.com>
2025-07-18 19:41:46 +0300

    Task done

---
+++ Try-catch/task.js
@@ -0,0 +1,50 @@
+//1
+
+function safeDivide(a, b) {
+    try {
+        if (b === 0) {
+            throw new Error("Деление на ноль невозможно");
+        }
+        return a / b;
+    } catch (error) {
+        console.log('Ошибка:', error.message);
+    }
+}
+
+console.log(safeDivide(10, 5));
+console.log(safeDivide(10, 0));
+
+//2
+
+function transfromJSON(jsonString) {
+    try {
+      const obj = JSON.parse(jsonString);
+      return obj;
+    } catch (error) {
+      console.error('Ошибка при парсинге JSON:', error.message);
+    }
+  }
+
+const json1 = '{"name": "Pupa", salary: 3000}';
+const json2 = '{"name": "Lupa", "salary": 300000}';
+
+console.log(transfromJSON(json1));
+console.log(transfromJSON(json2));
+
+/* 3. Напиши функцию `checkAccess`, которая принимает возраст пользователя. Если возраст меньше 18, функция должна бросать ошибку с сообщением 
+"Доступ запрещен". Используйте `try...catch` для обработки ошибок и выведи сообщение об ошибке в консоль. */
+
+function checkAccess(age){
+    try {
+        if (age < 18) {
+            throw new Error;
+        }
+        console.log('Доступ разрешен') 
+        
+    } catch (error) {
+        console.error('Доступ запрещен', error.message);
+    }
+}
+
+checkAccess(17);
+checkAccess(20);
