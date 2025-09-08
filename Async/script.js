d8f95a2c6093b6295b0a136b7fcd4d8bc0f1117b
icmstsan <penelopa.armstrong@gmail.com>
2025-09-08 17:13:39 +0300

    fixed

--- Async/script.js
+++ Async/script.js
@@ -29,27 +29,33 @@ showReminder(); */
 //3
 
 let intervalId = null;
-        const startBtn = document.getElementById('startBtn');
-        const delayInput = document.getElementById('delay');
-        const textInput = document.getElementById('text');
+const startBtn = document.getElementById('startBtn');
+const delayInput = document.getElementById('delay');
+const textInput = document.getElementById('text');
 
-        startBtn.addEventListener('click', function() {
-            if (intervalId) {
-                // Если таймер уже работает - останавливаем
-                clearInterval(intervalId);
-                intervalId = null;
-                startBtn.textContent = 'Начать';
-                console.log('Таймер остановлен');
-            } else {
-                // Если таймер не работает - запускаем
-                const delay = parseInt(delayInput.value);
-                const text = textInput.value;
-                
-                intervalId = setInterval(() => {
-                    console.log(text);
-                }, delay);
-                
-                startBtn.textContent = 'Остановить';
-                console.log('Таймер запущен');
-            }
-        });
+startBtn.addEventListener('click', function () {
+    if (intervalId) {
+        // Если таймер уже работает - останавливаем
+        clearInterval(intervalId);
+        intervalId = null;
+        startBtn.textContent = 'Начать';
+        console.log('Таймер остановлен');
+    } else {
+        // Если таймер не работает - запускаем
+        const delay = parseInt(delayInput.value);
+        const text = textInput.value;
+
+        if (delay < 1) {  //добавлена проверка
+            alert('Ошибка: введите корректное значение');
+            console.error('Ошибка: некорректное значение delay:', delayInput.value);
+            return;
+        }
+
+        intervalId = setInterval(() => {
+            console.log(text);
+        }, delay);
+
+        startBtn.textContent = 'Остановить';
+        console.log('Таймер запущен');
+    }
+});
